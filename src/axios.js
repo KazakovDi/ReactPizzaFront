aef43b0f96ebca166dab418f6810f966163f9529
KazakovDi <diman.ua68@gmail.com>
2022-12-19 16:33:48 +0200

    alert added

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from "axios"
 const instance = axios.create({
     baseURL: "https://react-pizza-m8ct.onrender.com"
 })
-
+// http://localhost:5000
+// https://react-pizza-m8ct.onrender.com
 instance.interceptors.request.use(config => {
     config.headers.Authorization = window.localStorage.getItem("token")
     return config
